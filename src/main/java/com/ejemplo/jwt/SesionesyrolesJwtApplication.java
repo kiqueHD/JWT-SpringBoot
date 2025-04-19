555353ddd6cbbbaba1121e4be1678942316ebe0d
Enrique <soyenriqueh@hotmail.com>
2025-04-19 17:02:38 +0200

    primer commit

---
+++ src/main/java/com/ejemplo/jwt/SesionesyrolesJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesionesyrolesJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesionesyrolesJwtApplication.class, args);
+	}
+
+}
